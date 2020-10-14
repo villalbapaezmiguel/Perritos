887b04041826665573d915bee9e3492dba2c3b89
villalbapaezmiguel <villalbapaezmiguel@gmail.com>
2020-10-14 17:32:40 -0300

    subiendo los primeros alrchivos de Perritos

--- js/perritos.js
+++ js/perritos.js
@@ -1,25 +1,95 @@
 function crearTabla() {
 
     var tablaPerritos = document.getElementById("tablaPerritos");
+    var fila = tablaPerritos.insertRow(0);//crea los espacios para las filas
 
-    for (var i = 0; i < datosDePerritos.length; i++) {
+    var columnaTamanio = fila.insertCell(0);//rellena los espacios de las filas
+    var columnaRaza = fila.insertCell(1);
+    var columnaEdad = fila.insertCell(2);
+    var columnaFoto = fila.insertCell(3);
+    var columnaDiscapacidad = fila.insertCell(4);
+    var columnaHistoria = fila.insertCell(5);
 
-        //Fila de títulos
-        var fila = tablaPerritos.insertRow(0);
+    columnaTamanio.innerHTML = "tamanio";//el relleno
+    columnaRaza.innerHTML = "raza";
+    columnaEdad.innerHTML = "edad";
+    columnaFoto.innerHTML = "foto";
+    columnaDiscapacidad.innerHTML = "discapacidad";
+    columnaHistoria.innerHTML = "historia";
 
-        var columnaTamanio = fila.insertCell(0);
-        var columnaRaza = fila.insertCell(1);
-        var columnaEdad = fila.insertCell(2);
-        var columnaFoto = fila.insertCell(3);
-        var columnaDiscapacidad = fila.insertCell(4);
-        var columnaHistoria = fila.insertCell(5);
+    let numeroDeFila = 1;
 
-        columnaTamanio.innerHTML = "tamanio";
-        columnaRaza.innerHTML = "raza";
-        columnaEdad.innerHTML = "edad";
-        columnaFoto.innerHTML = "foto";
-        columnaDiscapacidad.innerHTML = "discapacidad";
-        columnaHistoria.innerHTML = "historia";
-        //Fin fila de títulos
+    for (var posicion = 0;
+        posicion < datosDePerritos.length;
+        posicion++) {
+
+        let filaDatos = tablaPerritos.insertRow(numeroDeFila);
+
+        let columnaTamanioDatos = filaDatos.insertCell(0);
+        let columnaRazaDatos = filaDatos.insertCell(1);
+        let columnaEdadDatos = filaDatos.insertCell(2);
+        let columnaFotoDatos = filaDatos.insertCell(3);
+        let columnaDiscapacidadDatos = filaDatos.insertCell(4);
+        let columnaHistoriaDatos = filaDatos.insertCell(5);
+
+        columnaTamanioDatos.innerHTML = datosDePerritos[posicion].tamanio;
+        columnaRazaDatos.innerHTML = datosDePerritos[posicion].raza;
+        columnaEdadDatos.innerHTML = datosDePerritos[posicion].edad;
+        columnaFotoDatos.innerHTML = datosDePerritos[posicion].foto;
+        columnaDiscapacidadDatos.innerHTML = datosDePerritos[posicion].discapacidad;
+        columnaHistoriaDatos.innerHTML = datosDePerritos[posicion].historia;
+
+        numeroDeFila++;
+    }
+}
+
+function filtroDeTmanio() {
+    let filtroTamanioDePerrito = document.getElementById("tamanio").value;
+
+    var tablaPerritos = document.getElementById("tablaPerritos");
+    tablaPerritos.innerHTML = "";
+
+    var fila = tablaPerritos.insertRow(0);//crea los espacios para las filas
+
+    var columnaTamanio = fila.insertCell(0);//rellena los espacios de las filas
+    var columnaRaza = fila.insertCell(1);
+    var columnaEdad = fila.insertCell(2);
+    var columnaFoto = fila.insertCell(3);
+    var columnaDiscapacidad = fila.insertCell(4);
+    var columnaHistoria = fila.insertCell(5);
+
+    columnaTamanio.innerHTML = "tamanio";//el relleno
+    columnaRaza.innerHTML = "raza";
+    columnaEdad.innerHTML = "edad";
+    columnaFoto.innerHTML = "foto";
+    columnaDiscapacidad.innerHTML = "discapacidad";
+    columnaHistoria.innerHTML = "historia";
+
+    let numeroDeFila = 1;
+
+    for (var posicion = 0;
+        posicion < datosDePerritos.length;
+        posicion++) {
+
+        let filaDatos = tablaPerritos.insertRow(numeroDeFila);
+
+        let columnaTamanioDatos = filaDatos.insertCell(0);
+        let columnaRazaDatos = filaDatos.insertCell(1);
+        let columnaEdadDatos = filaDatos.insertCell(2);
+        let columnaFotoDatos = filaDatos.insertCell(3);
+        let columnaDiscapacidadDatos = filaDatos.insertCell(4);
+        let columnaHistoriaDatos = filaDatos.insertCell(5);
+
+        let tamanio = datosDePerritos[posicion].tamanio;
+        
+        if (filtroTamanioDePerrito == tamanio || filtroTamanioDePerrito === "mostrar") {
+            columnaTamanioDatos.innerHTML = tamanio;
+            columnaRazaDatos.innerHTML = datosDePerritos[posicion].raza;
+            columnaEdadDatos.innerHTML = datosDePerritos[posicion].edad;
+            columnaFotoDatos.innerHTML = datosDePerritos[posicion].foto;
+            columnaDiscapacidadDatos.innerHTML = datosDePerritos[posicion].discapacidad;
+            columnaHistoriaDatos.innerHTML = datosDePerritos[posicion].historia;
+        }
+        numeroDeFila++;
     }
 }
--- json/datos.js
+++ json/datos.js
@@ -1,10 +1,50 @@
 var datosDePerritos = [
     {
-        "tamanio": "mediano",
-        "raza": "",
-        "edad": 0,
+        "tamanio": "pequeño",
+        "raza": "Bichón Frisé",
+        "edad": 8,
         "foto": "",
-        "discapacidad" :"",
-        "historia":""
+        "discapacidad" :"ninguna",
+        "historia":"la a la ala"
+    },
+    {
+        "tamanio": "pequeño",
+        "raza": "Bichón Maltés",
+        "edad": 5,
+        "foto": "",
+        "discapacidad" :"ninguna",
+        "historia":"zaraza"
+    },
+    {
+        "tamanio": "mediano",
+        "raza": "Bulldog inglés.",
+        "edad": 7,
+        "foto": "",
+        "discapacidad" :"ninguna",
+        "historia":"perdido y no reclamado"
+    },
+    {
+        "tamanio": "mediano",
+        "raza": "Dálmata.",
+        "edad": 10,
+        "foto": "",
+        "discapacidad" :"ciego",
+        "historia":"dueños murieron"
+    },
+    {
+        "tamanio": "grande",
+        "raza": "San bernardo",
+        "edad": 15,
+        "foto": "",
+        "discapacidad" :"cadera",
+        "historia":"por terminar"
+    },
+    {
+        "tamanio": "grande",
+        "raza": "pastor alemán",
+        "edad": 4,
+        "foto": "",
+        "discapacidad" :"ninguna",
+        "historia":"con la ovejas"
     }
 ]
